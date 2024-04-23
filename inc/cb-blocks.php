3530b7d4635b5e6692fc9f2f066e5d0c041c07be
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-04-23 14:24:02 +0100

    people

--- inc/cb-blocks.php
+++ inc/cb-blocks.php
@@ -92,6 +92,15 @@ function acf_blocks()
             'mode'	=> 'edit',
             'supports' => array('mode' => false, 'anchor' => true),
         ));
+        acf_register_block_type(array(
+            'name'				=> 'cb_people',
+            'title'				=> __('CB People'),
+            'category'			=> 'layout',
+            'icon'				=> 'cover-image',
+            'render_template'	=> 'page-templates/blocks/cb_people.php',
+            'mode'	=> 'edit',
+            'supports' => array('mode' => false, 'anchor' => true),
+        ));
     }
 }
 add_action('acf/init', 'acf_blocks');
