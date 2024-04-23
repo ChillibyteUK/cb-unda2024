b96fbe6d0218e3e34171e94fa9f8ded026f7afea
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-04-23 17:22:48 +0100

    Update cb_cta.php

--- page-templates/blocks/cb_cta.php
+++ page-templates/blocks/cb_cta.php
@@ -1,4 +1,7 @@
-<section class="cta py-5">
+<?php
+$class = $block['className'] ?? null;
+?>
+<section class="cta py-5 <?=$class?>">
     <div class="container-xl d-flex flex-wrap justify-content-center align-items-center gap-4">
         <div class="h2 mb-0">Get a no-obligation, free quote</div>
         <a href="/contact-us/" class="btn btn-secondary">Start a Quote</a>
