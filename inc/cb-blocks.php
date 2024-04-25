d82346c1b5d13ddae38d7731c10f9e755d43b7a9
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-04-25 13:22:32 +0100

    faq

--- inc/cb-blocks.php
+++ inc/cb-blocks.php
@@ -137,6 +137,15 @@ function acf_blocks()
             'mode'	=> 'edit',
             'supports' => array('mode' => false, 'anchor' => true),
         ));
+        acf_register_block_type(array(
+            'name'				=> 'cb_faq',
+            'title'				=> __('CB FAQ'),
+            'category'			=> 'layout',
+            'icon'				=> 'cover-image',
+            'render_template'	=> 'page-templates/blocks/cb_faq.php',
+            'mode'	=> 'edit',
+            'supports' => array('mode' => false, 'anchor' => true),
+        ));
     }
 }
 add_action('acf/init', 'acf_blocks');
