24e682b51cea7b7f390617d74250bb6f1f6e462d
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-11-15 14:56:59 +0000

    Update cb_home_hero.php

--- page-templates/blocks/cb_home_hero.php
+++ page-templates/blocks/cb_home_hero.php
@@ -2,7 +2,7 @@
     <div class="container-xl text-center">
         <h1 data-aos="fade-up"><?=get_field('title')?></h1>
         <div class="home_hero__content" data-aos="fade-up" data-aos-delay="100"><?=get_field('content')?></div>
-        <a href="/contact-us/" class="btn btn-primary" data-aos="fade-up" data-aos-delay="200">Start a Quote</a>
+        <a href="/start-a-quote/" class="btn btn-primary" data-aos="fade-up" data-aos-delay="200">Start a Quote</a>
     </div>
     <div class="home_hero__layers">
         <img src="<?=get_stylesheet_directory_uri()?>/img/hero/layer-7.svg" id="l7" alt="">
