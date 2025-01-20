80faf80a6340ea8b52d90f81927fee8dce134e4d
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2025-01-20 16:20:53 +0000

    Update cb_contact.php

--- page-templates/blocks/cb_contact.php
+++ page-templates/blocks/cb_contact.php
@@ -1,6 +1,6 @@
 <div class="container-xl pb-5">
     <div class="row gy-5 pb-5">
-        <div class="col-md-4">
+        <div class="col-md-6">
             <h2>Contact Unda</h2>
             <ul class="fa-ul">
                 <li class="mb-2"><span class="fa-li"><i class="fa-solid fa-phone"></i></span> <?=contact_phone()?></li>
@@ -10,8 +10,9 @@
             <h3>Connect</h3>
             <?=social_icons('fa-2x')?>
         </div>
-        <div class="col-md-8">
-            <iframe src="<?=get_field('maps_url','options')?>" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <div class="col-md-6">
+            <?=do_shortcode('[gravityform id="2" title="false"]')?>
         </div>
     </div>
 </div>
+<iframe src="<?=get_field('maps_url','options')?>" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
