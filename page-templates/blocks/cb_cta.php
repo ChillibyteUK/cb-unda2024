9887fbc80c407342fe2cadb932146910d4bde151
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2025-01-22 09:26:25 +0000

    Update cb_cta.php

--- page-templates/blocks/cb_cta.php
+++ page-templates/blocks/cb_cta.php
@@ -9,7 +9,7 @@ if ( $template == 'page-templates/sidebar.php' ) {
     <div class="container-xl d-flex flex-wrap justify-content-center align-items-center gap-4">
         <div>
             <div class="h2 mb-0">Get a no-obligation, free quote</div>
-            <p>One of our experienced Flood Risk Consultants will get back to you within 60 minutes</p>
+            <p class="text-white">One of our experienced Flood Risk Consultants will get back to you within 60 minutes</p>
         </div>
         <a href="/contact-us/" class="btn btn-secondary">Get in Touch</a>
     </div>
