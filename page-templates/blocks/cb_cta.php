bcc3a64fe808a23ea729283a739fa308215ba027
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2025-01-22 09:30:09 +0000

    Update cb_cta.php

--- page-templates/blocks/cb_cta.php
+++ page-templates/blocks/cb_cta.php
@@ -6,7 +6,7 @@ if ( $template == 'page-templates/sidebar.php' ) {
 }
 ?>
 <section class="cta py-5 <?=$class?>">
-    <div class="container-xl d-flex flex-wrap justify-content-center align-items-center gap-4">
+    <div class="container-xl d-flex flex-wrap justify-content-center align-items-center" style="column-gap:1.5rem;row-gap:0.75rem;">
         <div>
             <div class="h2 mb-0">Get a no-obligation, free quote</div>
             <p class="text-white">One of our experienced Flood Risk Consultants will get back to you within 60 minutes</p>
